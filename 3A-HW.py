f5de3b28126fe4f456d09464b2679c5414f21be7
Josh L. Doman <32210532+JoshLDoman@users.noreply.github.com>
2020-02-02 03:58:15 -0800

    Update 3A-HW.py

--- 3A-HW.py
+++ 3A-HW.py
@@ -1,13 +1,31 @@
-income = input("Type income: ")
-expense = input("Type expense: ")
-profit = int(income) - int(expense)
-
-def person ():
-    if income >= 5000:
-        print("You're a high income earner.")
+#create function for income bracket with if-else statement
+def IncomeBracket (income):
+    if income >= 5000: 
+        return "High income earner"
     elif income >= 3000 and income < 5000:
-        print("You're an average income earner.")
+        return "Average income earner"
     else:
-        print("You're a low income earner.")
-        
-print("Your profit is: ", profit)
+        return "Low income earner"
+#create function for profit
+def Profit(income, expense):
+  return income - expense
+#declare variables 
+names = []
+profits = []
+choice = "y"
+#construct requirements for user input with control loops
+while choice == "y":
+  name = input("Enter name: ");
+  income = int(input("Type income: "))
+  expense = int(input("Type expense: "))
+ 
+  profit = Profit(income, expense)
+  print("Your profit is: ", profit)
+ 
+  names.append(name)
+  profits.append(profit)
+  choice = input("\nDo you want to input another entry? (y/n)")
+  print("\n")
+ 
+for i in range(len(names)):
+  print(f"Name: {names[i]}\tProfit: {profits[i]}\tIncome Bracket: {IncomeBracket(profits[i])}")
